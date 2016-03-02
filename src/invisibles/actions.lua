15f44ecae7aae6598622c85e039f14db7318aeef
Peter DeMartini <thepeterdemartini@gmail.com>
2016-03-02 06:47:54 -0700

    end jump on collision

--- src/invisibles/actions.lua
+++ src/invisibles/actions.lua
@@ -9,7 +9,10 @@ function Actions.new(chad)
   self.onCollision = function(event)
     if event.phase == "began" then
       local obj1Name, obj2Name = event.object1.name, event.object2.name
-      -- print("collision", obj1Name, obj2Name)
+      print("collision", obj1Name, obj2Name)
+      if obj1Name == 'solid' and obj2Name == 'chad' then
+        chad.actionEndJump()
+      end
     end
   end
 
@@ -17,16 +20,16 @@ function Actions.new(chad)
     if event.phase == "began" then
       print("screen touch, jumping")
       chad.actionJump()
-      timer.performWithDelay(700, chad.actionEndJump)
+      -- timer.performWithDelay(700, chad.actionEndJump)
     end
     return true
   end
 
   self.destroy = function()
-    -- Runtime:removeEventListener("collision", self.onCollision)
+    Runtime:removeEventListener("collision", self.onCollision)
     Runtime:removeEventListener("touch", self.onScreenTouch)
   end
-  -- Runtime:addEventListener("collision", self.onCollision)
+  Runtime:addEventListener("collision", self.onCollision)
   Runtime:addEventListener("touch", self.onScreenTouch)
 
   return self;
