3401e5e79465c7a4583c1d747df1d71909d53364
Peter DeMartini <thepeterdemartini@gmail.com>
2016-03-16 16:08:22 -0700

    handle android assets

--- src/image-path.lua
+++ src/image-path.lua
@@ -1,11 +1,16 @@
 function splitfilename(strfilename)
 	return string.match(strfilename, "(.-)([^\\]-([^\\%.]+))$")
 end
+local platform = system.getInfo("platformName")
 return function(rawFile)
   path,filename,extension = splitfilename(rawFile)
-  print('filename', path, filename, extension)
   if extension == 'jpg' then
     return 'images/build/jpgs/'..filename
   end
-  return 'images/build/ios/'..filename
+	local folderName = 'ios'
+	if platform == 'Android' then
+		local densityName = system.getInfo("androidDisplayDensityName")
+		folderName = 'drawable-'..densityName
+	end
+  return 'images/build/'..folderName..'/'..filename
 end
